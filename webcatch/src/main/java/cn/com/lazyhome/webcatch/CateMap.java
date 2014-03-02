3890d968f85b107ce3fc23592f1b68e68daa4650
rainbow <dchrainbow@gmail.com>
2014-03-02 19:39:23 +0800

    增加类别存储

---
+++ webcatch/src/main/java/cn/com/lazyhome/webcatch/CateMap.java
@@ -0,0 +1,17 @@
+package cn.com.lazyhome.webcatch;
+
+public class CateMap {
+	/**
+	 * key
+	 */
+	private String id;
+	/**
+	 * ÀàÃû
+	 */
+	private String catename;
+	/**
+	 * post URL
+	 */
+	private String posturl;
+
+}
---
+++ webcatch/src/main/java/cn/com/lazyhome/webcatch/Category.java
@@ -0,0 +1,16 @@
+package cn.com.lazyhome.webcatch;
+
+public class Category {
+	/**
+	 * key
+	 */
+	private String id;
+	/**
+	 * 类名
+	 */
+	private String catename;
+	/**
+	 * 类的URL
+	 */
+	private String url;
+}
